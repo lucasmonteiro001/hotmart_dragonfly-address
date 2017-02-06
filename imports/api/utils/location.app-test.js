f62437733ad6510d318296dc8c2933830550f37f
Lucas Monteiro <lucasmonteiro001@gmail.com>
2017-02-06 10:52:35 -0200

    Testes de location funcionando

---
+++ imports/api/utils/location.app-test.js
@@ -0,0 +1,41 @@
+/**
+ * Created by lucas on 2/6/17.
+ */
+import { Meteor } from 'meteor/meteor';
+import { assert } from 'meteor/practicalmeteor:chai';
+import { getLatLng } from './location';
+
+if(Meteor.isAppTest && Meteor.isServer) {
+
+    const data = {
+        zipCode: '32115-190',
+        lat: -19.8930573,
+        lng: -44.0258416
+    }
+
+    describe('Location helper', function () {
+
+        it('Get correct latitude and longitude', function (done) {
+
+            getLatLng(data.zipCode)
+                .then( function ({latitude, longitude}) {
+
+                    assert.equal(data.lat, latitude);
+
+                    assert.equal(data.lng , longitude);
+
+                    done();
+                })
+                .catch( function (error) {
+
+                    assert.isUndefined(error);
+
+                    done()
+                });
+
+        });
+
+    });
+}
+
+
