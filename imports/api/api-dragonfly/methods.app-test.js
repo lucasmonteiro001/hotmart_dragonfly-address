ff6dcd76e29e84b626a21ac46c4f380b97339578
Lucas Monteiro <lucasmonteiro001@gmail.com>
2017-02-07 17:32:41 -0200

    Teste funcionando

--- imports/api/api-dragonfly/methods.app-test.js
+++ imports/api/api-dragonfly/methods.app-test.js
@@ -6,9 +6,47 @@ import {assert} from 'meteor/practicalmeteor:chai';
 
 if (Meteor.isAppTest && Meteor.isServer) {
 
+    const test = function ({label, method, assertFunction, errorMsg}) {
+
+        return (
+            it(label, function () {
+
+                return (new Promise(function (resolve, reject) {
+
+                    Meteor.call(method, function (err, result) {
+
+                        if (err) {
+                            reject(err);
+                        }
+                        else {
+                            resolve(result);
+                        }
+                    });
+                }))
+                    .then(function (result) {
+                        assertFunction(result);
+                    })
+                    .catch(function (err) {
+                        assert.isNull(err, errorMsg + ' | ' + err.reason);
+                    });
+            })
+        )
+            ;
+    };
+
     describe('Methods', function () {
 
-      it('Get authentication token to login', function () {
+        test({
+            label: 'Testing authentication',
+            method: 'dragonfly-login',
+            assertFunction: function (res) {
+                console.log('chamando assert')
+                assert.isNotNull(res);
+            },
+            errorMsg: 'Deu erro'
+        });
+
+        /*it('Get authentication token to login', function () {
 
             return (new Promise(function (resolve, reject) {
 
@@ -30,7 +68,7 @@ if (Meteor.isAppTest && Meteor.isServer) {
                     assert.isNull(err);
                 });
 
-        });
+        });*/
 
     });
 }
